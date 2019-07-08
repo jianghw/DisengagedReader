292cec7fe992960f5179443a23df5e9e8c1e7acf
jianghw <hongwei.jiang@zhangmen.com>
2019-07-08 19:48:45 +0800

    升级

--- jetpackLib/src/androidTest/java/com/magnificent/jianghw/jetpacklib/ExampleInstrumentedTest.java
+++ jetpackLib/src/androidTest/java/com/magnificent/jianghw/jetpacklib/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.android.lib.am;
+package com.magnificent.jianghw.jetpacklib;
 
 import android.content.Context;
 import androidx.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.android.server.am.test", appContext.getPackageName());
+        assertEquals("com.magnificent.jianghw.jetpacklib.test", appContext.getPackageName());
     }
 }
